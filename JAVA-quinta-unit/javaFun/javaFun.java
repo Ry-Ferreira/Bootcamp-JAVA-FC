827af67a63f3e32b65591735e40d1a1c64db9e52
Ry-Ferreira <ryferreira1@outlook.es>
2023-07-12 17:17:13 -0300

    primer-ejercicios-JAVA

---
+++ JAVA-quinta-unit/javaFun/javaFun.java
@@ -0,0 +1,8 @@
+public class javaFun {
+    public static void main(String[] args) {
+
+        System.out.println("Mi nombre es Ryan Ferreira");
+        System.out.println("Tengo 21 anios de edad.");
+        System.out.println("Mi ciudad es Montevideo, cerro");
+    }
+}
